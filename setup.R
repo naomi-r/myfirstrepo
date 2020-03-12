ecaa239f32904872058740bdf7ed0f57f7abdbba
naomi-r <z5113625@unsw.edu.au>
2020-03-12 02:42:14 +0000

    introduce myself

---
+++ setup.R
@@ -0,0 +1,9 @@
+#setup script - has to be setup once per project, like how a project has to have packages
+
+install.packages("tidyverse")
+install.packages("usethis")
+install.packages("remotes")
+# run these 3 lines 
+
+
+
